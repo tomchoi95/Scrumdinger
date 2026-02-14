88a62b6262d7068fa063c06a93db32dd84d22d9f
tomchoi <qjatn0545@naver.com>
2026-02-14 19:59:00 +0900

    feat: DailyScrum모델 추가

---
+++ Scrumdinger/Models/DailyScrum.swift
@@ -0,0 +1,16 @@
+//
+//  DailyScrum.swift
+//  Scrumdinger
+//
+//  Created by Tom Choi on 2/14/26.
+//
+
+import Foundation
+import ThemeKit
+
+struct DailyScrum {
+    var title: String
+    var attendees: [String]
+    var lengthInMinutes: Int
+    var theme: Theme
+}
