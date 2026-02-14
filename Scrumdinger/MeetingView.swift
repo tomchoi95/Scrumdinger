46b2eaccd084135e369bb128c341735ba7040e3d
tomchoi <qjatn0545@naver.com>
2026-02-14 19:09:29 +0900

    chore: 이름을 변경합니다. - ContentView -> MeetingView

--- Scrumdinger/MeetingView.swift
+++ Scrumdinger/MeetingView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  MeetingView.swift
 //  Scrumdinger
 //
 //  Created by Tom Choi on 2/14/26.
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct MeetingView: View {
     var body: some View {
         VStack {
             Image(systemName: "globe")
@@ -20,5 +20,5 @@ struct ContentView: View {
 }
 
 #Preview {
-    ContentView()
+    MeetingView()
 }
