dcf88de78bb6464dee4e6fdd99edf9415f1d8cdc
tomchoi <qjatn0545@naver.com>
2026-02-15 02:38:03 +0900

    feat: 네비게이션 구현 - ScrumsView

--- Scrumdinger/ScrumsView.swift
+++ Scrumdinger/ScrumsView.swift
@@ -12,9 +12,22 @@ struct ScrumsView: View {
     let scrums: [DailyScrum]
     
     var body: some View {
-        List(scrums) { scrum in
-            CardView(scrum: scrum)
+        NavigationStack{
+            List(scrums) { scrum in
+                NavigationLink {
+                    Text(scrum.title)
+                } label: {
+                    CardView(scrum: scrum)
+                }
                 .listRowBackground(scrum.theme.mainColor)
+            }
+            .navigationTitle("Daily Scrums")
+            .toolbar {
+                Button(action: {}) {
+                    Image(systemName: "plus")
+                }
+                .accessibilityLabel("New Scrum")
+            }
         }
     }
 }
