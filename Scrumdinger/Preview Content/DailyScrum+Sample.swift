244d6a565dc2a64f0579ead03572efae4c49d33a
tomchoi <qjatn0545@naver.com>
2026-02-14 20:02:44 +0900

    feat: Preview용 스텁 모델 추가

---
+++ Scrumdinger/Preview Content/DailyScrum+Sample.swift
@@ -0,0 +1,26 @@
+//
+//  DailyScrum+Sample.swift
+//  Scrumdinger
+//
+//  Created by Tom Choi on 2/14/26.
+//
+
+import Foundation
+import ThemeKit
+
+extension DailyScrum {
+    static let sampleData: [DailyScrum] = [
+        DailyScrum(title: "Design",
+                           attendees: ["Cathy", "Daisy", "Simon", "Jonathan"],
+                           lengthInMinutes: 10,
+                           theme: .yellow),
+                DailyScrum(title: "App Dev",
+                           attendees: ["Katie", "Gray", "Euna", "Luis", "Darla"],
+                           lengthInMinutes: 5,
+                           theme: .orange),
+                DailyScrum(title: "Web Dev",
+                           attendees: ["Chella", "Chris", "Christina", "Eden", "Karla", "Lindsey", "Aga", "Chad", "Jenn", "Sarah"],
+                           lengthInMinutes: 5,
+                           theme: .poppy)
+    ]
+}
