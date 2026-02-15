0f7e258ea9e998dcb0bc261c51b9c7556104c186
tomchoi <qjatn0545@naver.com>
2026-02-15 23:17:47 +0900

    feat(History): 스크럼 기록 모델 추가

---
+++ Scrumdinger/Models/History.swift
@@ -0,0 +1,20 @@
+//
+//  History.swift
+//  Scrumdinger
+//
+//  Created by Tom Choi on 2/15/26.
+//
+
+import Foundation
+
+struct History: Identifiable {
+    let id: UUID
+    let date: Date
+    var attendees: [DailyScrum.Attendee]
+    
+    init(id: UUID = UUID(), date: Date = Date(), attendees: [DailyScrum.Attendee]) {
+        self.id = id
+        self.date = date
+        self.attendees = attendees
+    }
+}
