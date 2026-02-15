8f62cfddbc5c90886947cdba8317abe98cfec8d8
tomchoi <qjatn0545@naver.com>
2026-02-15 21:23:17 +0900

    feat(MeetingView): 스크럼 타이머 추가 및 UI 수정

--- Scrumdinger/Views/MeetingView.swift
+++ Scrumdinger/Views/MeetingView.swift
@@ -7,31 +7,22 @@
 
 import SwiftUI
 import ThemeKit
+import TimerKit
 
 struct MeetingView: View {
     @Binding var scrum: DailyScrum
+    @State var scrumTimer = ScrumTimer()
+    
     var body: some View {
         ZStack {
             RoundedRectangle(cornerRadius: 16)
                 .fill(scrum.theme.mainColor)
             VStack {
-                ProgressView(value: 5, total: 15)
-                HStack {
-                    VStack(alignment: .leading) {
-                        Text("Seconds Elapsed")
-                            .font(.caption)
-                        Label("300", systemImage: "hourglass.tophalf.fill")
-                    }
-                    Spacer()
-                    VStack(alignment: .trailing) {
-                        Text("Seconds Remaining")
-                            .font(.caption)
-                        Label("600", systemImage: "hourglass.bottomhalf.fill")
-                    }
-                }
-                .accessibilityElement(children: .ignore)
-                .accessibilityLabel("Time remaining")
-                .accessibilityValue("10 minutes")
+                MeetingHeaderView(
+                    secondsElapsed: scrumTimer.secondsElapsed,
+                    secondsRemaining: scrumTimer.secondsRemaining,
+                    theme: scrum.theme
+                )
                 Circle()
                     .strokeBorder(lineWidth: 24)
                 HStack {
