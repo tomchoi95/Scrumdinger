9820aa5fe53627d3c46f48539c1b56a8930b665d
tomchoi <qjatn0545@naver.com>
2026-02-17 16:51:31 +0900

    feat(MeetingView): 음성 인식 기능 추가 및 타이머 뷰 수정

--- Scrumdinger/Views/MeetingTimerView.swift
+++ Scrumdinger/Views/MeetingTimerView.swift
@@ -11,6 +11,7 @@ import TimerKit
 
 struct MeetingTimerView: View {
     let speakers: [ScrumTimer.Speaker]
+    let isRecording: Bool
     let theme: Theme
     
     private var currentSpeaker: String {
@@ -25,6 +26,10 @@ struct MeetingTimerView: View {
                     Text(currentSpeaker)
                         .font(.title)
                     Text("is Speaking")
+                    Image(systemName: isRecording ? "mic" : "mic.slash")
+                        .font(.title)
+                        .padding(.top)
+                        .accessibilityLabel(isRecording ? "with transcription" : "without transcription")
                 }
                 .accessibilityElement(children: .combine)
                 .foregroundStyle(theme.accentColor)
@@ -47,5 +52,5 @@ struct MeetingTimerView: View {
         ScrumTimer.Speaker(name: "Bill", isCompleted: true),
         ScrumTimer.Speaker(name: "Cathy", isCompleted: false),
     ]
-    MeetingTimerView(speakers: speakers, theme: .yellow)
+    MeetingTimerView(speakers: speakers, isRecording: true, theme: .yellow)
 }
